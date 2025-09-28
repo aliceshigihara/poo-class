2f8d0e13c1252bac2ee32a3b14a29a0c918a2283
Alice <aliceshigihara@gmail.com>
2025-09-27 23:52:06 -0300

    Exercicio 2 concluído

--- exercicio2.exercicios.ifc/src/exercicio2/exercicios/ifc/Bicicleta.java
+++ exercicio2.exercicios.ifc/src/exercicio2/exercicios/ifc/Bicicleta.java
@@ -4,38 +4,41 @@ public class Bicicleta {
 
     private int Velocidade;
     private boolean Andando;
-    
-    public int getVelocidade() {
-        return Velocidade;
-    }
-    
-// construtor
-    public Bicicleta(int Velocidade, boolean Andando) {
+
+
+    public Bicicleta() {
         this.Velocidade = 0;
         this.Andando = false;
     }
 
-//método acelera
+    public int getVelocidade() {
+        return Velocidade;
+    }
+
+
     public void Acelerar(int delta) {
         if (delta > 0);
         Velocidade += delta;
         Andando = true;
         System.out.println("Acelerou +" + delta + "km/h. Velocidade que está percorrendo agora: " + Velocidade);
     }
-    
-//método de freiar
+
 
     public void Frear(int delta) {
         if (delta > 0);
-        Velocidade -= delta;
-        if (Velocidade <= 0) {
-            Velocidade = 0;
-            Andando = false;
-            System.out.println("Freou -" + delta + "km/h. A bicicleta parou.");
-        } else {
-            System.out.println("Não está freiando");
-
+        {
+            Velocidade -= delta;
+            if (Velocidade <= 0) {
+                Velocidade = 0;
+                Andando = false;
+                System.out.println("Freou -" + delta + "km/h. A bicicleta parou.");
+            } else {
+                System.out.println("Freou -" + delta + "km/h. Velocidade atual: " + Velocidade + "km/h");
+            }
         }
     }
-//fazer método de mostrar velocidade
+
+    public String EstadoAtual() {
+        return Andando ? "Andando" : "Parada";
+    }
 }
--- exercicio2.exercicios.ifc/src/exercicio2/exercicios/ifc/exercicio2.java
+++ exercicio2.exercicios.ifc/src/exercicio2/exercicios/ifc/exercicio2.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class exercicio2 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner;
-        Bicicleta bicicleta1 = new Bicicleta(Velocidade, Andando);
-        //mostrar o estado inicial da bicicleta
-        //Velocidade atual
-        // o quanto quer acelerar
-        // o quanto quer freiar
+        Scanner scanning = new Scanner(System.in);
+        Bicicleta bicicleta1 = new Bicicleta();
+        
+        System.out.println("Estado inicial da bicicleta: " + bicicleta1.EstadoAtual());
+        System.out.println("Velocidade atual da bicileta:" + bicicleta1.getVelocidade());
+
+        System.out.print("Quanto você quer acelerar?: ");
+        int acelerou = scanning.nextInt();
+        bicicleta1.Acelerar(acelerou);
+
+        System.out.print("Quanto você deseja freiar?: ");
+        int freiou = scanning.nextInt();
+        bicicleta1.Frear(freiou);
+
+        System.out.println("Sua velocidade final: " + bicicleta1.getVelocidade() + "km/h");
+        System.out.println("Estado atual: " + bicicleta1.EstadoAtual());
+
     }
 
-    
 }
